bca91027f2a8481367a7a07c1329a54a99421f97
Xavier OILLIC <xavieroillic@gmail.com>
2023-11-09 01:13:53 +0100

    Test de commit d'eclipse vers Github

---
+++ src/main/java/HomerSimpson.java
@@ -0,0 +1,19 @@
+
+public class HomerSimpson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static String drinkBeer (String string) {
+		return "I love beers and duff !";
+	}
+	public static String eatDonuts () {
+		return "Tooohhh, des donuts !";
+	}
+	//public static String drinkBeer(String valueOf) {
+		// TODO Auto-generated method stub
+		//return null;
+	//}
+	
+}
