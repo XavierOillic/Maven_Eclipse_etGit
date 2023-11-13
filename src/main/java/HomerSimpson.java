bf0545a66ee61c7dada2a7d4f2324787d0610f49
Xavier OILLIC <xavieroillic@gmail.com>
2023-11-13 10:54:57 +0100

    Corrections des erreurs, fichier dans le mauvais dossier Package. Méthodes demandant des parametres alors que non nécessaire.

--- src/main/java/HomerSimpson.java
+++ src/main/java/HomerSimpson.java
@@ -1,19 +1,11 @@
 
 public class HomerSimpson {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	public static String drinkBeer (String string) {
+	public static String drinkBeer () {
 		return "I love beers and duff !";
 	}
 	public static String eatDonuts () {
 		return "Tooohhh, des donuts !";
+
 	}
-	//public static String drinkBeer(String valueOf) {
-		// TODO Auto-generated method stub
-		//return null;
-	//}
-	
 }
---
+++ src/test/java/Tests_HomerSimpson.java
@@ -0,0 +1,20 @@
+
+
+import org.junit.jupiter.api.Test;
+
+public class Tests_HomerSimpson {
+		
+		@Test
+		public void Duff() {
+			//fail("Not yet implemented");
+			HomerSimpson.drinkBeer().contains("duff");
+		}
+
+		@Test
+		public void Donuts() {
+			//fail("Not yet implemented");
+			HomerSimpson.eatDonuts().contains("donuts");
+		}
+		
+
+	}
